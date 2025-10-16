9dcab20315a3efa82fbb1e6b8932ea58cac65246
aaronwalterdselga-rgb <aaronwalter.d.selga@isu.edu.ph>
2025-10-16 23:04:24 +0800

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,13 @@
+print("Task 1: The Basic Calculator")
+
+a = 207
+b = 5
+
+print("a =", a, ", b =", b)
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Floating-Point Division:", a / b)
+print("Integer Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
